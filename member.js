cf8368483ea29ffa2fedd10bdf8abb966b66c757
John Lopera <johnlope@gmail.com>
2023-11-14 20:02:13 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsMember(){
+    return {
+        restrict: 'E',
+        template: 'modules/skills/views/member.html',
+        controller: 'SkillsMemberController',
+        controllerAs :'vm',
+        bindToController: true,
+        scope: {
+            member: '='
+        }
+    }
+}
